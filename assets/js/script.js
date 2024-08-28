ef0f088a65a5cde82453a68f644a3f1d251a2b4d
Max Kaening <maximilian.kaening@gmail.com>
2024-08-28 12:03:49 +0000

    Add main function - click buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,40 @@
 /** DOM-VARIABLES **/
+/* Sections */
 const welcome_section = document.getElementById("welcome");
 const rules_section = document.getElementById("rules");
 const gameArea_section = document.getElementById("game-area");
 
-
+/** GAME-AREA **/
+/* Scores & Stats */
 const userScore = 0;
 const computerScore = 0;
 const userScore_span = document.getElementById("player-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
+/* Click & Play */
 const choices_div = document.getElementsByClassName(".choices");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 const reset_div = document.getElementById("reset");
- 
+
+
+function main() {
+    rock_div.addEventListener('click', function() {
+        game("rock");
+    })
+    
+    paper_div.addEventListener('click', function() {
+        game("paper");
+    })
+    
+    scissors_div.addEventListener('click', function() {
+        game("scissors");
+    })
+    
+    reset_div.addEventListener('click', function() {
+        game("reset");
+    })
+}
+
